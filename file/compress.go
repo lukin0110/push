33715b7121f0a663d4b6e92fce6fb620f5c12c96
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2016-11-17 13:39:01 +0100

    calculation function

--- file/compress.go
+++ file/compress.go
@@ -6,11 +6,64 @@ import (
     "io"
     "path/filepath"
     "strings"
+    "errors"
+    "fmt"
 )
 
+// Similar to: http://stackoverflow.com/questions/26050380/go-tracking-post-request-progress
+type ProgressWriter struct {
+    io.Writer
+    Reporter func(r int64)
+}
+
+func (pw *ProgressWriter) Write(p []byte) (n int, err error) {
+    n, err = pw.Writer.Write(p)
+    pw.Reporter(int64(n))
+    return
+}
+
+func CalculateSize(items ...string) (total int64, err error) {
+    total = 0
+
+    for _, value := range items {
+	var info os.FileInfo
+	info, err = os.Stat(value)
+	if err != nil {
+	    return
+	}
+
+	if !info.IsDir() {
+	    fmt.Println("BANAAN 2")
+	    total += info.Size()
+	} else {
+	    filepath.Walk(value, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+		    return err
+		}
+
+		info, err = os.Stat(path)
+		if err != nil {
+		    return err
+		}
+
+		if !info.IsDir() {
+		    total += info.Size()
+		}
+		return nil
+	    })
+	}
+    }
+
+    return
+}
+
+
 // http://blog.ralch.com/tutorial/golang-working-with-zip/
-// TODO: avoid added targetName itself, report func
-func ZippyMcZipface(targetName string, reporter func(r int64), items ...string) (err error) {
+func ZippyMcZipface(targetName string, reporter func(w int64), items ...string) (err error) {
+    if reporter == nil {
+	return errors.New("reporter function can't be nil")
+    }
+
     zipfile, err := os.Create(targetName)
     if err != nil {
 	return
@@ -52,6 +105,8 @@ func ZippyMcZipface(targetName string, reporter func(r int64), items ...string) 
 	    }
 
 	    writer, err := archive.CreateHeader(header)
+	    proxyWriter := &ProgressWriter{writer, reporter}
+
 	    if err != nil {
 		return err
 	    }
@@ -65,7 +120,7 @@ func ZippyMcZipface(targetName string, reporter func(r int64), items ...string) 
 		return err
 	    }
 	    defer file.Close()
-	    _, err = io.Copy(writer, file)
+	    _, err = io.Copy(proxyWriter, file)
 	    return err
 	})
     }
