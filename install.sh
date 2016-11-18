a1df744f2ed887e844a08de1fb8600c0b9400fcb
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2016-11-18 17:19:46 +0100

    building for arm

--- install.sh
+++ install.sh
@@ -9,15 +9,16 @@ readonly VERSION
 # system. Will exit with an error if it is not ours.
 #####################################################################
 # 'Our' push command should write to the stderr
-checker=$(push --unknown 2>&1 >/dev/null)
-
-if echo "$checker" | grep -q "flag provided but not defined: -unknown"; then
-    echo "Installing 'push'"
-else
-    echo "Error: could not install 'push'" 1>&2
-    echo "A 'push' command already exists in $(which push)" 1>&2
-	exit 1
-fi;
+#checker=$(push --unknown 2>&1 >/dev/null)
+#echo $checker
+#
+#if (echo "$checker" | grep -q "flag provided but not defined: -unknown"); then
+#    echo "Installing 'push'"
+#else
+#    echo "Error: could not install 'push'" 1>&2
+#    echo "A 'push' command already exists in $(which push)" 1>&2
+#	exit 1
+#fi;
 
 
 #####################################################################
@@ -37,9 +38,14 @@ case "$(uname -s)" in
     ;;
 
     Linux)
-        echo 'Linux'
         temp_file=$(mktemp)
-        curl -o "$temp_file" -L https://github.com/lukin0110/push/releases/download/$VERSION/push.x86.linux
+        arch=$(uname -p)
+        if [ "$arch" = "unknown" ]; then
+            arch="x86"
+        fi;
+
+        echo "Installing on Linux $arch"
+        curl -o "$temp_file" -L https://github.com/lukin0110/push/releases/download/$VERSION/push.$arch.linux
         mv "$temp_file" /usr/local/bin/push
         chmod 755 /usr/local/bin/push
         echo "Installed in /usr/local/bin/push"
